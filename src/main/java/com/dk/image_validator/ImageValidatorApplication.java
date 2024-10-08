5a30025407e7317987687fd6bc90d73c413e6d64
Dhruvik Rangani <dhruvik.rangani@argusoft.in>
2024-10-08 16:19:08 +0530

    initial-commit

---
+++ src/main/java/com/dk/image_validator/ImageValidatorApplication.java
@@ -0,0 +1,13 @@
+package com.dk.image_validator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageValidatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImageValidatorApplication.class, args);
+	}
+
+}
